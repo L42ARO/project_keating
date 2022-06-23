b39a7e9384909e64d609172ae84119708dc9faae
alvaro512 <alvaro512@usf.edu>
2022-06-23 02:34:16 -0500

    Potentially fixed main.exe error

--- backend/pkg/utils/utils.go
+++ backend/pkg/utils/utils.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"reflect"
+	"strings"
 )
 
 func SetHeaderJSON(w *http.ResponseWriter) {
@@ -16,4 +18,20 @@ func ParseBody(r *http.Request, x interface{}) {
 			return
 		}
 	}
+}
+func GetNonEmptyFields(s *interface{}) ([]string, []interface{}) {
+	fields := reflect.TypeOf(*s)
+	values := reflect.ValueOf(*s)
+	nonEmptyFields := []string{}
+	nonEmptyValues := []interface{}{}
+	for i := 0; i < fields.NumField(); i++ {
+		if fields.Field(i).Name == "Id" {
+			continue
+		}
+		if !reflect.DeepEqual(values.Field(i).Interface(), reflect.Zero(fields.Field(i).Type).Interface()) {
+			nonEmptyFields = append(nonEmptyFields, strings.ToLower(fields.Field(i).Name))
+			nonEmptyValues = append(nonEmptyValues, values.Field(i).Interface())
+		}
+	}
+	return nonEmptyFields, nonEmptyValues
 }
